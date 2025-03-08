071f5cfedd4a85a63dcba4ee2fd4ac5a6978cb21
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-08 11:16:50 +0000

    fix(deps): update required_provider to latest for deployable architectures (#375)

--- solutions/quickstart/version.tf
+++ solutions/quickstart/version.tf
@@ -4,11 +4,11 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.75.2"
+      version = "1.76.1"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.12.1"
+      version = "0.13.0"
     }
   }
 }
