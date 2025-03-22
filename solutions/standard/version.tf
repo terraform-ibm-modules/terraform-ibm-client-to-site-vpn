76f76bd1d156722cf1d28f1f97ccff45da1d91f2
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-22 04:22:13 +0000

    fix(deps): update terraform ibm to latest for deployable architectures (#382)

--- solutions/standard/version.tf
+++ solutions/standard/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.76.1"
+      version = "1.76.2"
     }
     time = {
       source  = "hashicorp/time"
