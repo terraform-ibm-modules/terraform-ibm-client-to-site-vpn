4b70105bd8f55a6c621ba27ffe37dd188b659a31
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-06-28 16:44:28 +0000

    fix(deps): update terraform dependencies

--- examples/ha-complete/main.tf
+++ examples/ha-complete/main.tf
@@ -54,7 +54,7 @@ module "private_secret_engine" {
   depends_on                = [module.secrets_manager]
   count                     = var.existing_sm_instance_guid == null ? 1 : 0
   source                    = "terraform-ibm-modules/secrets-manager-private-cert-engine/ibm"
-  version                   = "1.5.5"
+  version                   = "1.6.0"
   secrets_manager_guid      = local.sm_guid
   region                    = local.sm_region
   root_ca_name              = var.root_ca_name
@@ -71,7 +71,7 @@ module "private_secret_engine" {
 module "secrets_manager_private_certificate" {
   depends_on             = [module.private_secret_engine]
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.3.3"
+  version                = "1.4.0"
   cert_name              = "${var.prefix}-cts-vpn-private-cert"
   cert_description       = "an example private cert"
   cert_template          = var.certificate_template_name
@@ -91,7 +91,7 @@ module "secrets_manager_private_certificate" {
 # Minimal VPC for illustration purpose: 2 subnets across 2 availability zones
 module "basic_vpc" {
   source               = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version              = "7.25.3"
+  version              = "7.25.6"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   name                 = "vpc"
