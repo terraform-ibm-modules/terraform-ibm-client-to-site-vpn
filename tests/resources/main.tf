b5b00dd1eddc0e30f4f3e460e9ea138d19dd48fe
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-11 21:45:19 +0000

    fix(deps): update terraform dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source                 = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
-  version                = "8.3.0"
+  version                = "8.4.0"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
