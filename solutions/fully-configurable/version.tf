14d12824a153e0329290b6a1b5fb40bf8c00f7ba
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-02 20:20:58 +0000

    fix(deps): update terraform dependencies

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.80.4"
+      version = "1.81.0"
     }
     time = {
       source  = "hashicorp/time"
