f8df1f2dac096d67e4c5f38de359f85283e3f4be
akocbek <akocbek>
2025-08-27 11:39:13 +0100

    fix: improve DA user experience

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -1,14 +1,14 @@
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = module.existing_sm_crn_parser.region
-  alias            = "ibm-sm"
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = module.existing_sm_crn_parser.region
+  alias                 = "ibm-sm"
   visibility            = var.provider_visibility
   private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = local.vpc_region
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = local.vpc_region
   visibility            = var.provider_visibility
   private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
