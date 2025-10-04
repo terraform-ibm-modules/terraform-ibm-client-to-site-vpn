cb06eccf76dccd2446749493e03d1d70b7cb0082
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-04 04:13:27 +0100

    fix(deps): update dependencies (#509)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -30,7 +30,7 @@ module "secrets_manager_group" {
 # Create the private cert
 module "secrets_manager_private_certificate" {
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.4.7"
+  version                = "1.5.0"
   cert_name              = "${var.prefix}-cts-vpn-private-cert"
   cert_description       = "an example private cert"
   cert_template          = var.certificate_template_name
@@ -50,7 +50,7 @@ module "secrets_manager_private_certificate" {
 # Minimal VPC for illustration purpose: 1 subnet across 1 availability zone
 module "basic_vpc" {
   source               = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version              = "8.3.0"
+  version              = "8.5.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   name                 = "vpc"
