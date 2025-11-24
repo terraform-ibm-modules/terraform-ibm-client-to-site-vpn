b8f8b0c028c9a70dd96bfe35136b15e019104348
Aatreyee Mukherjee <mukherjeeaatreyee25@gmail.com>
2025-11-24 18:16:42 +0530

    feat: Add next-steps URL for the DA (#528)

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -26,3 +26,28 @@ output "vpn_id" {
   description = "Client to Site VPN ID"
   value       = module.vpn.vpn_server_id
 }
+
+output "next_steps_text" {
+  value       = "Now, you can access your VPN server."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to client-to-site VPN server"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/infrastructure/network/vpnServers/${module.existing_vpc_crn_parser.region}~${module.vpn.vpn_server_id}/overview"
+  description = "Primary URL for the Client-to-Site VPN instance"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about client-to-site VPN servers"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://cloud.ibm.com/docs/vpc?topic=vpc-vpn-client-to-site-overview&interface=ui"
+  description = "Secondary URL"
+}
