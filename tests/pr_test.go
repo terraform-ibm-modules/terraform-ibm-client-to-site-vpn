295e348df3a2af58fe5e167336983cc94e3efa7e
raagamounica <mounica.tellabati@ibm.com>
2025-11-25 18:56:15 +0530

    test: updating with schematics-upgrade test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -163,13 +163,13 @@ func TestFullyConfigurableSolutionInSchematics(t *testing.T) {
 	}
 }
 
-func TestRunSchematicUpgrade(t *testing.T) {
+// Upgrade test for "Fully configurable" DA variation in schematics
+func TestFullyConfigurableSolutionUpgrade(t *testing.T) {
 	t.Parallel()
 
 	// ------------------------------------------------------------------------------------------------------
 	// Create SLZ VPC, resource group first
 	// ------------------------------------------------------------------------------------------------------
-
 	prefix := fmt.Sprintf("cts-s-%s", strings.ToLower(random.UniqueId()))
 	realTerraformDir := "./resources"
 	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
@@ -189,7 +189,7 @@ func TestRunSchematicUpgrade(t *testing.T) {
 		Vars: map[string]interface{}{
 			"prefix":        prefix,
 			"region":        region,
-			"resource_tags": []string{"test-schematic"},
+			"resource_tags": []string{"test-schematic-upgrade"},
 		},
 		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
 		// This is the same as setting the -upgrade=true flag with terraform.
@@ -198,49 +198,44 @@ func TestRunSchematicUpgrade(t *testing.T) {
 
 	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
 	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	require.NoError(t, existErr, "Init and Apply of temp resources (SLZ VPC and Secrets Manager) failed")
 
-	if existErr != nil {
-		assert.True(t, existErr == nil, "Init and Apply of temp resources (SLZ VPC and Secrets Manager) failed")
-	} else {
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing:                    t,
+		Prefix:                     prefix,
+		TarIncludePatterns:         []string{fullyConfigurableFlavorDir + "/*.*", "*.tf"},
+		TemplateFolder:             fullyConfigurableFlavorDir,
+		ResourceGroup:              terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+		CheckApplyResultForUpgrade: true,
+		WaitJobCompleteMinutes:     60,
+		Region:                     region,
+		Tags:                       []string{"test-schematic"},
+		DeleteWorkspaceOnFail:      false,
+	})
 
-		options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
-			Testing: t,
-			Prefix:  prefix,
-			TarIncludePatterns: []string{
-				fullyConfigurableFlavorDir + "/*.*",
-				"*.tf",
-			},
-			ResourceGroup:          terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-			TemplateFolder:         fullyConfigurableFlavorDir,
-			Tags:                   []string{"test-schematic"},
-			DeleteWorkspaceOnFail:  false,
-			WaitJobCompleteMinutes: 60,
-			Region:                 region,
-		})
-		options.TerraformVars = []testschematic.TestSchematicTerraformVar{
-			{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
-			{Name: "prefix", Value: options.Prefix, DataType: "string"},
-			{Name: "existing_resource_group_name", Value: terraform.Output(t, existingTerraformOptions, "resource_group_name"), DataType: "string"},
-			{Name: "existing_secrets_manager_instance_crn", Value: permanentResources["secretsManagerCRN"], DataType: "string"},
-			{Name: "existing_vpc_crn", Value: terraform.Output(t, existingTerraformOptions, "management_vpc_crn"), DataType: "string"},
-			{Name: "private_cert_engine_config_root_ca_common_name", Value: fmt.Sprintf("%s%s", options.Prefix, ".com"), DataType: "string"},
-			{Name: "private_cert_engine_config_template_name", Value: permanentResources["privateCertTemplateName"], DataType: "string"},
-			{Name: "provider_visibility", Value: "public", DataType: "string"},
-			{Name: "remote_cidr", Value: "10.10.120.0/24", DataType: "string"},
-			{Name: "vpn_subnet_cidr_zone_1", Value: "10.10.40.0/24", DataType: "string"},
-			{Name: "vpn_subnet_cidr_zone_2", Value: "10.10.80.0/24", DataType: "string"},
-			{Name: "vpn_client_access_acl_ids", Value: []string{terraform.Output(t, existingTerraformOptions, "default_network_acl_id")}, DataType: "list(string)"},
-			{Name: "protocol", Value: "tcp", DataType: "string"},
-		}
-		err := options.RunSchematicUpgradeTest()
-		if !options.UpgradeTestSkipped {
-			assert.NoError(t, err, "Upgrade test should complete without errors")
-		}
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+		{Name: "existing_resource_group_name", Value: terraform.Output(t, existingTerraformOptions, "resource_group_name"), DataType: "string"},
+		{Name: "existing_secrets_manager_instance_crn", Value: permanentResources["secretsManagerCRN"], DataType: "string"},
+		{Name: "existing_vpc_crn", Value: terraform.Output(t, existingTerraformOptions, "management_vpc_crn"), DataType: "string"},
+		{Name: "private_cert_engine_config_root_ca_common_name", Value: fmt.Sprintf("%s%s", options.Prefix, ".com"), DataType: "string"},
+		{Name: "private_cert_engine_config_template_name", Value: permanentResources["privateCertTemplateName"], DataType: "string"},
+		{Name: "provider_visibility", Value: "public", DataType: "string"},
+		{Name: "remote_cidr", Value: "10.10.120.0/24", DataType: "string"},
+		{Name: "vpn_subnet_cidr_zone_1", Value: "10.10.40.0/24", DataType: "string"},
+		{Name: "vpn_subnet_cidr_zone_2", Value: "10.10.80.0/24", DataType: "string"},
+		{Name: "vpn_client_access_acl_ids", Value: []string{terraform.Output(t, existingTerraformOptions, "default_network_acl_id")}, DataType: "list(string)"},
+		{Name: "protocol", Value: "tcp", DataType: "string"},
 	}
 
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
+	err := options.RunSchematicUpgradeTest()
+	if !options.UpgradeTestSkipped {
+		assert.NoError(t, err, "Upgrade test should complete without errors")
+	}
+
+	// Clean up resources unless DO_NOT_DESTROY_ON_FAILURE is set
 	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
 	if t.Failed() && strings.ToLower(envVal) == "true" {
 		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
 	} else {
@@ -250,6 +245,7 @@ func TestRunSchematicUpgrade(t *testing.T) {
 		logger.Log(t, "END: Destroy (existing resources)")
 	}
 }
+
 func TestFullyConfigurableSolutionExistingResources(t *testing.T) {
 	t.Parallel()
 
