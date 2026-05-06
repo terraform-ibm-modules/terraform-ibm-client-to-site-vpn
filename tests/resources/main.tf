f34fbf8e4dbaf1822994d6dfe98c0335e46a10af
Vipin Kumar <77929205+vkuma17@users.noreply.github.com>
2026-05-06 11:49:34 +0530

    chore: set cos endpoint type to public for test resources (#548)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,13 +3,14 @@
 ##############################################################################
 
 module "landing_zone" {
-  source                 = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
-  version                = "8.8.0"
-  region                 = var.region
-  prefix                 = var.prefix
-  tags                   = var.resource_tags
-  enable_transit_gateway = false
-  add_atracker_route     = false
+  source                     = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
+  version                    = "8.17.0"
+  region                     = var.region
+  prefix                     = var.prefix
+  tags                       = var.resource_tags
+  enable_transit_gateway     = false
+  add_atracker_route         = false
+  existing_cos_endpoint_type = "public"
 }
 
 ################################################################################
