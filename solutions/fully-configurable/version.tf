ecacdb4592279d1a4b304c978fded0e830876562
Mukul Palit <mukul.palit@ibm.com>
2026-05-22 16:32:33 +0530

    fix(deps): deprecated terratest functions and add context (#551)

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,11 +4,11 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "2.0.2"
+      version = "2.1.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.13.1"
+      version = "0.14.0"
     }
   }
 }
