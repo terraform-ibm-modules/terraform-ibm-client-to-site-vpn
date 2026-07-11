331680e0814fe21d2eae8977c4c797770da80834
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-11 08:21:40 +0000

    fix(deps): update dependencies

--- examples/ha-complete/main.tf
+++ examples/ha-complete/main.tf
@@ -26,7 +26,7 @@ module "resource_group" {
 # Create a new SM instance if not using an existing one
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.15.9"
+  version              = "2.15.11"
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   resource_group_id    = module.resource_group.resource_group_id
   region               = local.sm_region
@@ -39,7 +39,7 @@ module "secrets_manager" {
 # Create a secret group to place the certificate in
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.5.3"
+  version                  = "1.5.4"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certs"
@@ -95,7 +95,7 @@ module "secrets_manager_private_certificate" {
 # Minimal VPC for illustration purpose: 2 subnets across 2 availability zones
 module "basic_vpc" {
   source               = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version              = "9.1.0"
+  version              = "9.1.1"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   name                 = "vpc"
